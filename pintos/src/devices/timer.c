037cae422cb0625edc865aa9f317fca385e54f27
yj2218 <yj2218@hanmail.net>
2013-10-18 16:40:46 +0900

    Update timer.c

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -101,8 +101,9 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  //while (timer_elapsed (start) < ticks) 
+  //  thread_yield ();  modified , while -loop 제거
+  thread_sleep(ticks);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be

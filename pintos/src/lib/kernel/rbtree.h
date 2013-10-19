af71b442abaeb9f615b479822f846150f616dadb
yj2218 <yj2218@hanmail.net>
2013-10-19 11:35:48 +0900

    Update rbtree.h
    insert 후에 해당노드 리턴하게 수정

--- pintos/src/lib/kernel/rbtree.h
+++ pintos/src/lib/kernel/rbtree.h
@@ -1,4 +1,3 @@
-
 #ifndef _RBTREE_H_
 
 #include <assert.h>
@@ -21,9 +20,9 @@ typedef struct rbtree_t {
 
 typedef int (*compare_func)(void* left, void* right);
 
-rbtree rbtree_create();
+rbtree rbtree_create(); //rb tree를 만들고, 이 리턴값에 tree->root 에 루트노드가 할당된다.
 void* rbtree_lookup(rbtree t, void* key, compare_func compare);
-void rbtree_insert(rbtree t, void* key, void* value, compare_func compare);
+rbtree_node_t * rbtree_insert(rbtree t, void* key, void* value, compare_func compare); //이 인서트를 홀용,
 void rbtree_delete(rbtree t, void* key, compare_func compare);
 
 #endif

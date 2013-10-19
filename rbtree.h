78e821ff8eebac0fb61b89526198db81a52b0ee8
yj2218 <yj2218@hanmail.net>
2013-10-19 09:52:33 +0900

    Create rbtree.h

---
+++ rbtree.h
@@ -0,0 +1,26 @@
+
+#ifndef _RBTREE_H_
+
+enum rbtree_node_color { RED, BLACK };
+
+typedef struct rbtree_node_t {
+    void* key;
+    void* value;
+    struct rbtree_node_t* left;
+    struct rbtree_node_t* right;
+    struct rbtree_node_t* parent;
+    enum rbtree_node_color color;
+} *rbtree_node;
+
+typedef struct rbtree_t {
+    rbtree_node root;
+} *rbtree;
+
+typedef int (*compare_func)(void* left, void* right);
+
+rbtree rbtree_create();
+void* rbtree_lookup(rbtree t, void* key, compare_func compare);
+void rbtree_insert(rbtree t, void* key, void* value, compare_func compare);
+void rbtree_delete(rbtree t, void* key, compare_func compare);
+
+#endif
